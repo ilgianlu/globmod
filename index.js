67ac1a3f057508835668ab65fdd0bc7b0dd414c7
Gianluca Magni <gmagni@softeam.it>
2022-05-03 12:22:26 +0200

    mod2 update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const mod1 = require('./mod1');
+const mod2 = require('./mod2');
 
 function main() {
     const glob = {
-        mod1
+        mod1,
+        mod2
     };
 
     console.log(glob);
